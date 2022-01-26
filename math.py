48c94857e204464f43d02f3f4bc8890426f6dc65
ujjwalapatil123 <ujjwala_patil@persistent.com>
2022-01-26 13:07:51 +0530

    added fn headers locally

--- math.py
+++ math.py
@@ -1,9 +1,13 @@
-
+  
+  #Add implementation
 def add(x,y):
     pass
+    #Substract implementation
 def substract(x,y):
     pass
+    #Multiply implementation
 def multiply(x,y):
     pass
+    #divide implementation
 def divide(x,y):
     pass
